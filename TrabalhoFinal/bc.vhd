19bffd22cd9ebf8bf053be9bae0ed2faf978f049
MarkiCast <36992814+MarkiCast@users.noreply.github.com>
2021-04-28 15:24:53 -0300

    update 0.3
    estrutura geral pronta
    falta ROM pra compilar

--- TrabalhoFinal/bc.vhd
+++ TrabalhoFinal/bc.vhd
@@ -4,84 +4,112 @@ USE ieee.std_logic_1164.all;
 ENTITY bc IS
 PORT (clk : IN STD_LOGIC;
 		opcode: in std_logic_vector(3 downto 0);
-		enPC, enA, enB, enOut: OUT STD_LOGIC
+		enPC, enA, enB, enOut, enOp: OUT STD_LOGIC
 		);
 END bc;
 
 ARCHITECTURE behave OF bc IS
-	--TYPE state_type IS (S0, S1, S2, S3, S4, S5 );
-	--SIGNAL state: state_type;
+	TYPE state_type IS (S0, S1, S2, S3, S4, S5, S6, S7, S8);
+	SIGNAL state: state_type;
 BEGIN
 	-- Logica de proximo estado (e registrador de estado)
---	PROCESS (clk, opcode)
---	BEGIN
---		if(Reset = '1') THEN
---			state <= S0 ;
---		ELSIF (clk'EVENT AND clk = '1') THEN
---			CASE state IS
---				WHEN S0 => if (inicio = '1') THEN
---									state <= S1;
---								end if;
---				WHEN S1 => state <= S2; 
---							  
---
---				WHEN S2 => if ((Az or Bz) = '1') THEN
---									state <= S5;
---							  else 
---									state <= S3;
---							  end if;
---
---				WHEN S3 => state <= S4;	
---
---				WHEN S4 => state <= S2;
---
---				WHEN S5 => state <= S0;	
---
---			END CASE;
---		END IF;
---	END PROCESS;
---	
---	-- Logica de saida
---	PROCESS (state)
---	BEGIN
---		CASE state IS
---			WHEN S0 =>
---				ini <= '0';
---				CA <= '0';
---				dec <= '0';
---				CP <= '0';
---				pronto <= '0';
---				
---			WHEN S1 => dec <= '0';
---						  CP <= '0';
---						  pronto <= '0';
---						  ini <='1';
---						  CA <='1';
---				
---			WHEN S2 => CP <= '0';
---						  ini <= '0';
---						  CA <= '0';
---						  dec <= '0';
---						  pronto <= '0';
---				
---			WHEN S3 =>dec <= '0'; 
---				       CA <= '0';
---						 CP <= '1';
---						 pronto <= '0';
---						 ini <='0';
---
---			WHEN S4 =>ini <= '0';
---						CA <= '1';
---						dec <= '1';
---						CP <= '0';
---						pronto <= '0';
---				
---			WHEN S5 =>ini <= '0';
---						CA <= '0';
---						dec <= '0';
---						CP <= '0';
---						pronto <= '1';
---
---		END CASE;
---	END PROCESS
+	PROCESS (clk, opcode)
+	BEGIN
+		IF rising_edge(clk) THEN
+			CASE state IS
+				WHEN S0 => state <= S1;
+				
+				WHEN S1 => state <= S2; 				  
+
+				WHEN S2 => if (opcode = "0010" or opcode =  "0011" or opcode =  "0100") THEN
+									state <= S3;
+							  else 
+									state <= S5;
+							  end if;
+
+				WHEN S3 => state <= S4;
+			
+				when s4 => state <= s0;
+				
+				when s5 => state <= s6;
+				
+				when s6 => state <= s7;
+				
+				when s7 => state <= s8;
+				
+				when s8 => state <= s0;
+				
+			END CASE;
+		END IF;
+	END PROCESS;
+	
+	-- Logica de saida
+	PROCESS (state)
+	BEGIN
+		CASE state IS
+			WHEN S0 =>
+				enPC <= '0';
+				enA <= '0';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '1';
+				
+			WHEN S1 => 
+				enPC <= '1';
+				enA <= '0';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '0';
+				
+			WHEN S2 => 
+				enPC <= '0';
+				enA <= '0';
+				enB <= '0';
+				enOp <= '1';
+				enOut <= '0';
+				
+			WHEN S3 =>
+				enPC <= '1';
+				enA <= '0';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '0';
+
+			WHEN S4 =>
+				enPC <= '0';
+				enA <= '1';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '0';
+				
+			WHEN S5 =>
+				enPC <= '1';
+				enA <= '0';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '0';
+			
+			WHEN S6 =>
+				enPC <= '0';
+				enA <= '1';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '0';	
+			
+			WHEN S7 =>
+				enPC <= '1';
+				enA <= '0';
+				enB <= '0';
+				enOp <= '0';
+				enOut <= '0';	
+				
+			WHEN S8 =>
+				enPC <= '0';
+				enA <= '0';
+				enB <= '1';
+				enOp <= '0';
+				enOut <= '0';
+				
+		END CASE;
+	END PROCESS;
 END behave;
